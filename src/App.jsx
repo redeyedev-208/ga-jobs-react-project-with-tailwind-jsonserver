4569a53f3e18698cb9dc8bb327260c3d48f21071
gilaguilar <redeyedev.208@gmail.com>
2024-04-21 17:32:02 -0600

    main layout done, fixed a11y main section needed, now tackling state, will adjust color scheme later to pass contrast failure

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,17 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import HomeCards from './components/HomeCards';
 import JobListings from './components/JobListings';
+import ViewAllJobs from './components/ViewAllJobs';
 
 const App = () => {
   return (
-    <>
+    <main>
       <Navbar />
       <Hero />
       <HomeCards />
       <JobListings />
-
-      <section className='m-auto max-w-lg my-10 px-6'>
-        <a
-          href='jobs.html'
-          className='block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700'
-        >
-          View All Jobs
-        </a>
-      </section>
-    </>
+      <ViewAllJobs />
+    </main>
   );
 };
 
