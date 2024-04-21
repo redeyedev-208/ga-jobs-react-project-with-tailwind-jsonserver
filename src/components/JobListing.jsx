f7167d990192e9bba8ca8e10d8f5d4c49c1c5600
gilaguilar <redeyedev.208@gmail.com>
2024-04-21 17:59:16 -0600

    added icon package, and wrapped up home page, time for routing

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaMapMarker } from 'react-icons/fa';
 
 const JobListing = ({ job }) => {
+  const [showFullInfo, setShowFullInfo] = useState(false);
+  let description = job.description;
+
+  if (!showFullInfo) {
+    description = description.substring(0, 90) + '...';
+  }
+
+  const toggleShowFullInfo = () => {
+    setShowFullInfo((prevState) => !prevState);
+  };
+
   return (
     <div className='bg-white rounded-xl shadow-md relative'>
       <div className='p-4'>
@@ -9,7 +21,14 @@ const JobListing = ({ job }) => {
           <h3 className='text-xl font-bold'>{job.title}</h3>
         </div>
 
-        <div className='mb-5'>{job.description}</div>
+        <div className='mb-5'>{description}</div>
+
+        <button
+          onClick={toggleShowFullInfo}
+          className='text-indigo-500 mb-5 hover:text-indigo-600'
+        >
+          {showFullInfo ? 'Less' : 'More'}
+        </button>
 
         <h3 className='text-indigo-500 mb-2'>{job.salary} / Year</h3>
 
@@ -17,7 +36,7 @@ const JobListing = ({ job }) => {
 
         <div className='flex flex-col lg:flex-row justify-between mb-4'>
           <div className='text-orange-700 mb-3'>
-            <i className='fa-solid fa-location-dot text-lg'></i>
+            <FaMapMarker className='inline text-lg mb-1 mr-1' />
             {job.location}
           </div>
           <a
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
               <img
                 className='h-10 w-auto'
                 src={logo}
-                alt='React Jobs'
+                alt='Jobs Project with react logo'
               />
               <span className='hidden md:block text-white text-2xl font-bold ml-2'>
                 React Jobs
