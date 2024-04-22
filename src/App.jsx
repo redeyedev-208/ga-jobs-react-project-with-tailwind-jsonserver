c84e30854946d4e416c5b7af76aabee072cb72a6
gilaguilar <redeyedev.208@gmail.com>
2024-04-21 18:28:24 -0600

    added rrdom along with setup for main page, time to tackle jobs page and additional pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import HomeCards from './components/HomeCards';
-import JobListings from './components/JobListings';
-import ViewAllJobs from './components/ViewAllJobs';
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+import HomePage from './pages/HomePage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path='/'
+      element={<MainLayout />}
+    >
+      <Route
+        index
+        element={<HomePage />}
+      />
+    </Route>,
+  ),
+);
 
 const App = () => {
   return (
     <main>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
+      <RouterProvider router={router} />
     </main>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,6 @@ const Navbar = () => {
       <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
         <div className='flex h-20 items-center justify-between'>
           <div className='flex flex-1 items-center justify-center md:items-stretch md:justify-start'>
-            {/* <!-- Logo --> */}
             <a
               className='flex flex-shrink-0 items-center mr-4'
               href='/index.html'
